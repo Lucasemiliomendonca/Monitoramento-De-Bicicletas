c61ce887d1d15cd2585be925debc40e2a9a95fad
Lucasemiliomendonca <mendoncae.metrocasa@gmail.com>
2025-07-22 13:43:44 -0300

    Monitoramento de Bicicletas

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Bicicleta
+{
+    static void Main()
+    {
+       
+        string modelo = Console.ReadLine()!;
+
+        
+        string nivelStr = Console.ReadLine()!;
+        int nivelBateria = int.Parse(nivelStr);
+
+        
+        BicicletaInterna bicicleta = new BicicletaInterna(modelo, nivelBateria);
+
+        
+        Console.WriteLine(bicicleta.ObterMensagem());
+    }
+}
+
+
+class BicicletaInterna
+{
+    private string modelo;
+    private int nivelBateria;
+
+    
+    
+    public BicicletaInterna (string modelo, int nivelBateria)
+    {
+      this.modelo = modelo;
+      this.nivelBateria = nivelBateria;
+    }
+
+    
+    public decimal CalcularDistancia()
+    {
+      return nivelBateria * 0.5M;
+    }
+
+    public string ObterMensagem()
+    {
+        return $"{modelo}: Distancia estimada = {CalcularDistancia():F1} km";
+    }
+}
+
+
+
+
